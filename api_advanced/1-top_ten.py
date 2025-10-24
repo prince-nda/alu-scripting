e9c9933209867d799eeda3efb3f517802b26e029
prince-da <p.ndahiro1@alustudent.com>
2025-10-24 11:07:04 +0000

    make another changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
 """
-Module to fetch top ten hot posts from a subreddit
+Reddit API module
 """
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
+    """Print the titles of the first 10 hot posts for a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "python:agent:v1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     
-    Args:
-        subreddit (str): The subreddit to query
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'python:subreddit.hot.posts:v1.0'}
-    params = {'limit': 10}
+    if response.status_code != 200:
+        print("None")
+        return
     
-    response = requests.get(url, headers=headers, params=params,
-                          allow_redirects=False)
-    
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            if posts:
-                for post in posts:
-                    print(post['data']['title'])
-            else:
-                print("None")
-        except (KeyError, ValueError):
-            print("None")
-    else:
+    try:
+        data = response.json()
+        posts = data["data"]["children"][:10]
+        for post in posts:
+            print(post["data"]["title"])
+    except Exception:
         print("None")
