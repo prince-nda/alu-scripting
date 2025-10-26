40ed4f9336014e1ba7c8c25b85496993bc54d8e9
prince-da <p.ndahiro1@alustudent.com>
2025-10-26 12:50:36 +0000

    update this file

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""Module to fetch top ten hot posts from a subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a given subreddit."""
+    """Print titles of top 10 hot posts in subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "MyAPI/0.0.1"}
     params = {"limit": 10}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+        url, headers=headers, params=params, allow_redirects=False
+    )
     
     if response.status_code == 200:
         data = response.json().get("data")
         children = data.get("children") if data else []
-        
-        if children:
+        if len(children) > 0:
             for child in children:
                 print(child.get("data").get("title"))
         else:
